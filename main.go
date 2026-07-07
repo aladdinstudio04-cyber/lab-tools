24f48849c46b61acbbb299b6e5fadb4f1c54bf91
aladdinstudio04-cyber <aladdinstudio04@gmail.com>
2026-07-07 15:49:08 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-    "io"
     "log"
     "net"
+    "os"
 )
 
 func main() {
-    listener, err := net.Listen("tcp", ":4444")
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "4444" // fallback for local testing
+    }
+
+    listener, err := net.Listen("tcp", ":"+port)
     if err != nil {
         log.Fatal("Failed to bind:", err)
     }
-    log.Println("TCP C2 listening on :4444")
+    log.Println("TCP C2 listening on :" + port)
+
     for {
         conn, err := listener.Accept()
         if err != nil {
@@ -25,10 +31,5 @@ func main() {
 func handleConnection(client net.Conn) {
     defer client.Close()
     log.Println("New connection from:", client.RemoteAddr())
-
-    // Here you can either:
-    // 1. Send a static message back
-    // 2. Or pipe it to a local listener (if you want full interactivity)
-    // For simplicity, we just echo back a greeting
     client.Write([]byte("Connected to Render C2!\n"))
 }
